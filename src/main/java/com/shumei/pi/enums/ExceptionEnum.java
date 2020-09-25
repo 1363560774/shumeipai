9c7449ff854628d317d437715037a8e1c34ee5db
kai <1363560774@qq.com>
2020-09-25 23:00:09 +0800

    pwm调试

--- src/main/java/com/shumei/pi/enums/ExceptionEnum.java
+++ src/main/java/com/shumei/pi/enums/ExceptionEnum.java
@@ -17,7 +17,7 @@ public enum ExceptionEnum {
      */
     PWM_PULSE_RANGE_IS_TOO_LARGE(400, "脉冲范围过大,不能超过600"),
     PWM_PULSE_WIDTH_IS_TOO_LARGE(400, "等待脉冲完成时长不能超过1000"),
-    PWM_PULSE_COUNT_IS_TOO_LARGE(400, "脉冲数不得超过25"),
+    PWM_PULSE_COUNT_IS_TOO_LARGE(400, "脉冲数必须在5到25之间"),
     PWM_PULSE_VELOCITY_IS_TOO_LARGE(400, "脉冲速率不能过大,不能小于脉冲数的十倍"),
     PWM_PULSE_TUNING_WIDTH_IS_TOO_LARGE(400, "脉冲范围过大,微调和范围相加不能超过3000"),
     ;
