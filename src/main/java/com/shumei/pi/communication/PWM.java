620dac2cc6ac3f347f7f6b0d4cbe220c0b645bc2
kai <1363560774@qq.com>
2020-09-25 22:51:15 +0800

    pwm调试

--- src/main/java/com/shumei/pi/communication/PWM.java
+++ src/main/java/com/shumei/pi/communication/PWM.java
@@ -27,23 +27,18 @@ public class PWM {
 
     /**
      * 设置pwm脉冲
-     * @param range 范围
-     * @param pulseWidth 脉冲宽度(睡眠时长控制)
-     * @param pulse 脉冲
-     * @param direction 方向
-     * @param velocity 速度
-     * @param fineAdjustmentCompensation 微调补偿
+     * 测试参数如下
+     * @param range 范围 600
+     * @param pulseWidth 脉冲宽度(睡眠时长控制) 500
+     * @param pulse 脉冲 5-25
      * @return 消耗时长(毫秒)
      * @throws InterruptedException 线程睡眠异常
      */
     @GetMapping("/testPWM")
     public ResponseEntity<Object> testPWM(@RequestParam("range") Integer range,
                                           @RequestParam("pulseWidth") Integer pulseWidth,
-                                          @RequestParam("pulse") Integer pulse,
-                                          @RequestParam("direction") Boolean direction,
-                                          @RequestParam("velocity") Integer velocity,
-                                          @RequestParam("fineAdjustmentCompensation") Integer fineAdjustmentCompensation) throws InterruptedException {
-        String parametersCaveat = checkParameters(range, pulseWidth, pulse, velocity, fineAdjustmentCompensation);
+                                          @RequestParam("pulse") Integer pulse) throws InterruptedException {
+        String parametersCaveat = checkParameters(range, pulseWidth, pulse);
         if (!parametersCaveat.equals("SUCCESS")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(parametersCaveat);
         }
@@ -55,65 +50,24 @@ public class PWM {
         GpioPinPwmOutput pwm = gpio.provisionSoftPwmOutputPin(pin);
         pwm.setPwmRange(range);
         long startTime = System.currentTimeMillis();
-        long totalRange = range + fineAdjustmentCompensation;
-        if (direction == null || direction) {
-            while (pulse < totalRange){
-                TimeUnit.MILLISECONDS.sleep(pulseWidth);
-                pulse += velocity;
-                pwm.setPwm(pulse);
-                System.out.println(pulse);
-            }
-        } else {
-            while (pulse + range > totalRange){
-                TimeUnit.MILLISECONDS.sleep(pulseWidth);
-                pulse -= velocity;
-                pwm.setPwm(pulse);
-                System.out.println(pulse);
-            }
-        }
+        pwm.setPwm(pulse);
+        TimeUnit.MILLISECONDS.sleep(pulseWidth);
         long ensTime = System.currentTimeMillis();
         gpio.shutdown();
         gpio.unprovisionPin(pwm);
-
-
-//        String parametersCaveat = checkParameters(range, pulseWidth, pulse, velocity, fineAdjustmentCompensation);
-//        Assert.check(parametersCaveat.equals(""), parametersCaveat);
-//        long startTime = System.currentTimeMillis();
-//        long totalRange = range + fineAdjustmentCompensation;
-//        if (direction == null || direction) {
-//            while (pulse < totalRange){
-//                TimeUnit.MILLISECONDS.sleep(pulseWidth);
-//                pulse += velocity;
-//                System.out.println(pulse);
-//            }
-//        } else {
-//            while (pulse + range > totalRange){
-//                TimeUnit.MILLISECONDS.sleep(pulseWidth);
-//                pulse -= velocity;
-//                System.out.println(pulse);
-//            }
-//        }
-//        long ensTime = System.currentTimeMillis();
         log.info("消耗时长:{}毫秒", ensTime-startTime);
         return ResponseEntity.status(HttpStatus.OK).body(ensTime-startTime);
     }
 
-    private String checkParameters(Integer range, Integer pulseWidth, Integer pulse,
-                                   Integer velocity, Integer fineAdjustmentCompensation) {
-        if (range == null || range > 3000) {
-            return ExceptionEnum.PWM_PULSE_COUNT_IS_TOO_LARGE.getMsg();
+    private String checkParameters(Integer range, Integer pulseWidth, Integer pulse) {
+        if (range == null || range > 600) {
+            return ExceptionEnum.PWM_PULSE_RANGE_IS_TOO_LARGE.getMsg();
         }
         if (pulseWidth == null || pulseWidth > 1000) {
             return ExceptionEnum.PWM_PULSE_WIDTH_IS_TOO_LARGE.getMsg();
         }
-        if (pulse == null || pulse > range) {
+        if (pulse == null || pulse > 25) {
             return ExceptionEnum.PWM_PULSE_COUNT_IS_TOO_LARGE.getMsg();
-        }
-        if (velocity == null || pulse/velocity < 10) {
-            return ExceptionEnum.PWM_PULSE_VELOCITY_IS_TOO_LARGE.getMsg();
-        }
-        if (fineAdjustmentCompensation == null || fineAdjustmentCompensation + range > 3000) {
-            return ExceptionEnum.PWM_PULSE_TUNING_WIDTH_IS_TOO_LARGE.getMsg();
         }
         return PinEnum.PWM_SUCCESS.getMsg();
     }
--- src/main/java/com/shumei/pi/enums/ExceptionEnum.java
+++ src/main/java/com/shumei/pi/enums/ExceptionEnum.java
@@ -15,9 +15,9 @@ public enum ExceptionEnum {
     /**
      * 异常状态
      */
-    PWM_PULSE_RANGE_IS_TOO_LARGE(400, "脉冲范围过大,不能超过3000"),
-    PWM_PULSE_WIDTH_IS_TOO_LARGE(400, "脉冲宽度过大"),
-    PWM_PULSE_COUNT_IS_TOO_LARGE(400, "脉冲数不得超过脉冲范围"),
+    PWM_PULSE_RANGE_IS_TOO_LARGE(400, "脉冲范围过大,不能超过600"),
+    PWM_PULSE_WIDTH_IS_TOO_LARGE(400, "等待脉冲完成时长不能超过1000"),
+    PWM_PULSE_COUNT_IS_TOO_LARGE(400, "脉冲数不得超过25"),
     PWM_PULSE_VELOCITY_IS_TOO_LARGE(400, "脉冲速率不能过大,不能小于脉冲数的十倍"),
     PWM_PULSE_TUNING_WIDTH_IS_TOO_LARGE(400, "脉冲范围过大,微调和范围相加不能超过3000"),
     ;
